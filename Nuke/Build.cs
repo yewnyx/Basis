b559ccaf0722a8692a88274fbc30e6a609cb184b
yewnyx <yewnyx@yewnyx.xyz>
2024-08-10 16:32:42 -0700

    nuke build scripts

---
+++ Nuke/Build.cs
@@ -0,0 +1,109 @@
+using System;
+using System.IO.Compression;
+using System.Threading;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tools.Docker;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.Unity;
+using xyz.yewnyx.build.Nuke;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+
+namespace xyz.yewnyx.build;
+
+class Build : NukeBuild
+{
+    public static int Main () => Execute<Build>(x => x.PlayerBuild);
+
+    const string ProjectName = "Basis";
+
+    [Parameter("Build Profile")] BuildProfile BuildProfile;
+
+    [GitVersion] readonly GitVersion GitVersion;
+
+    Target SwitchProfile => _ => _
+        .Requires(() => BuildProfile)
+        .Executes(() =>
+        {
+            UnityTasks.Unity(_ => _
+                .SetHubVersion("6000.0.14f1")
+                .SetProjectPath(RootDirectory)
+                .SetExecuteMethod("xyz.yewnyx.build.BuildScript.SwitchProfile")
+                .AddCustomArguments("-activeBuildProfile", BuildProfile.BuildProfilePath)
+            );
+        });
+
+    Target PlayerBuild => _ => _
+        .Requires(() => BuildProfile)
+        .DependsOn(SwitchProfile)
+        .Executes(() =>
+        {
+            var outputFolder = BuildProfile.GetOutputFolder(RootDirectory);
+            outputFolder.CreateOrCleanDirectory();
+
+            var shipFolder = BuildProfile.GetShipFolder(RootDirectory);
+            shipFolder.CreateOrCleanDirectory();
+
+            var dontShipFolder = BuildProfile.GetDontShipFolder(RootDirectory);
+            dontShipFolder.CreateOrCleanDirectory();
+
+            var outputPath = BuildProfile.GetExecutablePath(RootDirectory, ProjectName);
+
+            UnityTasks.Unity(_ => _
+                .SetHubVersion("6000.0.14f1")
+                .SetProjectPath(RootDirectory)
+                .EnableBatchMode()
+                .EnableNoGraphics()
+                .SetExecuteMethod("xyz.yewnyx.build.BuildScript.Build")
+                .AddCustomArguments("-activeBuildProfile", BuildProfile.BuildProfilePath)
+                .AddCustomArguments("-branch", GitVersion.BranchName)
+                .AddCustomArguments("-commit", GitVersion.ShortSha)
+                .AddCustomArguments("-outputPath", outputPath));
+
+            var dontShipDirectories = (outputFolder / "Ship").GlobDirectories("*_BurstDebugInformation_DoNotShip",
+                "*_BackUpThisFolder_ButDontShipItWithYourGame");
+            foreach (var directory in dontShipDirectories) {
+                directory.MoveToDirectory(dontShipFolder);
+            }
+
+            var dontShipArchive = BuildProfile.GetDontShipArchive(RootDirectory, ProjectName, "zip");
+            dontShipFolder.ZipTo(dontShipArchive, compressionLevel: CompressionLevel.SmallestSize);
+            dontShipFolder.DeleteDirectory();
+        });
+
+    Target DockerBuild => _ => _
+        .TriggeredBy(PlayerBuild)
+        .Requires(() => BuildProfile)
+        .OnlyWhenDynamic(() => BuildProfile.IsLinuxServer)
+        .Executes(() =>
+        {
+            var dockerFile = RootDirectory / "Nuke"/ "Docker" / "Dockerfile";
+            var dockerIgnore = RootDirectory / "Nuke"/ "Docker" / ".dockerignore";
+
+            var dockerContext = TemporaryDirectory / "DockerContext" / ProjectName;
+            dockerContext.CreateOrCleanDirectory();
+
+            var shipFolder = BuildProfile.GetShipFolder(RootDirectory);
+            var dockerImage = ProjectName.Replace(" ", "-").ToLower();
+            var dockerTag = GitVersion.EscapedBranchName;
+
+            CopyFileToDirectory(dockerFile, dockerContext);
+            CopyFileToDirectory(dockerIgnore, dockerContext);
+            CopyDirectoryRecursively(shipFolder, dockerContext / "Game");
+
+            DockerTasks.DockerBuild(_ => _
+                .SetFile(dockerFile)
+                .AddLabel($"org.opencontainers.image.version=\"{GitVersion.BranchName}-{GitVersion.ShortSha}\"")
+                .AddTag($"{dockerImage}:latest")
+                .AddTag($"{dockerImage}:{dockerTag}")
+                .SetPath(dockerContext)
+                .AddBuildArg($"SERVER_EXECUTABLE=\"{BuildProfile.GetExecutableFilename(ProjectName)}\"")
+            );
+            DockerTasks.DockerSave(_ => _
+                .SetImages($"{dockerImage}:{dockerTag}")
+                .SetOutput(BuildProfile.GetOutputFolder(RootDirectory) / ($"{dockerImage}-{dockerTag}.tar")));
+            dockerContext.DeleteDirectory();
+        });
+}
---
+++ Nuke/BuildProfile.cs
@@ -0,0 +1,85 @@
+using System;
+using System.ComponentModel;
+using System.IO;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+
+namespace xyz.yewnyx.build.Nuke;
+
+// Copied from https://github.com/yewnyx/UnityNukeExample/blob/main/Nuke/BuildProfile.cs, most build profiles won't
+// work unless added to the project.
+[TypeConverter(typeof(TypeConverter<BuildProfile>))]
+public class BuildProfile : Enumeration
+{
+    public static BuildProfile AndroidDev = new() { Value = "Android Dev" };
+    public static BuildProfile Android = new() { Value = "Android" };
+    public static BuildProfile iOSDev = new() { Value = "iOS Dev" };
+    public static BuildProfile iOS = new() { Value = "iOS" };
+    public static BuildProfile LinuxServerDev = new() { Value = "Linux Server Dev" };
+    public static BuildProfile LinuxServerMonoDev = new() { Value = "Linux Server Mono Dev" };
+    public static BuildProfile LinuxServer = new() { Value = "Linux Server" };
+    public static BuildProfile QuestDev = new() { Value = "Quest Dev" };
+    public static BuildProfile Quest = new() { Value = "Quest" };
+    public static BuildProfile WindowsDev = new() { Value = "Windows Dev" };
+    public static BuildProfile WindowsMonoDev = new() { Value = "Windows Mono Dev" };
+    public static BuildProfile WindowsServerDev = new() { Value = "Windows Server Dev" };
+    public static BuildProfile WindowsServerMonoDev = new() { Value = "Windows Server Mono Dev" };
+    public static BuildProfile WindowsServer = new() { Value = "Windows Server" };
+    public static BuildProfile Windows = new() { Value = "Windows" };
+
+    public static implicit operator string(BuildProfile buildProfile) => buildProfile.Value;
+
+    public string SpacelessValue => Value.Replace(" ", "");
+
+    public bool IsLinuxServer => this == LinuxServer || this == LinuxServerDev || this == LinuxServerMonoDev;
+
+    public string Tag => Value switch
+    {
+        "Android Dev" => "Dev",
+        "Android" => string.Empty,
+        "iOS Dev" => "Dev",
+        "iOS" => string.Empty,
+        "Linux Server Dev" => "DedicatedServerDev",
+        "Linux Server Mono Dev" => "DedicatedServerDevMono",
+        "Linux Server" => "DedicatedServer",
+        "Quest Dev" => "Dev",
+        "Quest" => string.Empty,
+        "Windows Dev" => "Dev",
+        "Windows Mono Dev" => "DevMono",
+        "Windows Server Dev" => "DedicatedServerDev",
+        "Windows Server Mono Dev" => "DedicatedServerDevMono",
+        "Windows Server" => "DedicatedServer",
+        "Windows" => string.Empty,
+        _ => throw new ArgumentOutOfRangeException()
+    };
+
+    public string Extension => Value switch
+    {
+        "Android Dev" => ".apk",
+        "Android" => ".apk",
+        "iOS Dev" => ".ipa",
+        "iOS" => ".ipa",
+        "Linux Server Dev" => ".x86_64",
+        "Linux Server Mono Dev" => ".x86_64",
+        "Linux Server" => ".x86_64",
+        "Quest Dev" => ".apk",
+        "Quest" => ".apk",
+        "Windows Dev" => ".exe",
+        "Windows Mono Dev" => ".exe",
+        "Windows Server Dev" => ".exe",
+        "Windows Server Mono Dev" => ".exe",
+        "Windows Server" => ".exe",
+        "Windows" => ".exe",
+        _ => throw new ArgumentOutOfRangeException()
+    };
+
+    public AbsolutePath GetOutputFolder(AbsolutePath rootDirectory) => rootDirectory / "Builds" / SpacelessValue;
+    public string GetExecutableFilename(string exeName) => $"{exeName}{Tag}{Extension}";
+    public AbsolutePath GetExecutablePath(AbsolutePath rootDirectory, string exeName) => GetShipFolder(rootDirectory) / GetExecutableFilename(exeName);
+    public AbsolutePath GetShipFolder(AbsolutePath rootDirectory) => GetOutputFolder(rootDirectory) / "Ship";
+    public AbsolutePath GetDontShipFolder(AbsolutePath rootDirectory) => GetOutputFolder(rootDirectory) / "DontShip";
+    public AbsolutePath GetDontShipArchive(AbsolutePath rootDirectory, string exeName, string archiveExtension) =>
+        GetOutputFolder(rootDirectory) / $"{exeName}{Tag}-{SpacelessValue}.{archiveExtension}";
+
+    public string BuildProfilePath => Path.Combine("Assets", "Settings", "Build Profiles", $"{Value}.asset");
+}
